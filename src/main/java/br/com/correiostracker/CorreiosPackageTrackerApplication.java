b1e1cce4029fb37d1f7474c6484355bbef8a286d
Eduardo Lopes <duh.lopes234@hotmail.com>
2022-09-01 10:38:58 -0300

    Initial Commit

---
+++ src/main/java/br/com/correiostracker/CorreiosPackageTrackerApplication.java
@@ -0,0 +1,13 @@
+package br.com.correiostracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorreiosPackageTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CorreiosPackageTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/correiostracker/CorreiosPackageTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.correiostracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CorreiosPackageTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
